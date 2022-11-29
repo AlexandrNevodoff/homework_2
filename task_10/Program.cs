5b5136ad3d9eec31d4efed7491d2546f21d8f7ea
Alexandr Nevodov <nevodoff@gmail.com>
2022-11-29 15:37:59 +0300

    init commit

---
+++ task_10/Program.cs
@@ -0,0 +1,11 @@
+﻿// task 10 Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.Clear ();
+Console.Write("Введите трехзначное число:");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a > 999 || a < 100) Console.WriteLine("Число не трехзначное");
+else
+{
+    while (a > 100)
+        a = a/10;
+        Console.WriteLine(a%10);
+}
---
+++ task_13/Program.cs
@@ -0,0 +1,12 @@
+﻿// task 13 Напишите программу, которая выводит третью цифру(c начала) заданного числа или сообщает, что третьей цифры нет.НЕ ИСПОЛЬЗОВАТЬ НУМЕРАЦИЮ СТРОК
+
+Console.Clear ();
+Console.Write("Введите число:");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a < 100) Console.WriteLine("Третьей цифры нет");
+else
+{
+    while (a > 1000)
+        a = a/10;
+        Console.WriteLine(a%10);
+}
